1005728d0d7b7915774e79f68b3a017bdbbfe4a6
mahdi <ssmmhh1382@gmail.com>
2021-09-05 19:13:00 +0430

    django content blocks and snippets

--- src/personal/views.py
+++ src/personal/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screan_view(request):
-    return render(request,"base.html",{})
+    return render(request,"personal/home.html",{})
