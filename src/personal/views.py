23d382cf3a7497b7172e69c9b0cdacec1cea2658
mahdi <ssmmhh1382@gmail.com>
2021-09-06 18:06:24 +0430

    passing variables to tempelate

--- src/personal/views.py
+++ src/personal/views.py
@@ -3,4 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_screan_view(request):
-    return render(request,"personal/home.html",{})
+    # ex) 1
+    # content = {}
+    # content["my_number"] = "sisad hezaro bist o char"
+
+    # ex) 2
+    # content = {
+    # "my_number": "charsad bist o char yea yea212",
+    # "khersi": "khersi is here \n his name is coli"
+    # }
+
+    # ex) 3
+    content = {}
+    listOfBooks = ["work harder","clean code","best program","khajebe"]
+    listOfBooks.append("TAMARKOZ")
+
+    content["listOfBooks"] = listOfBooks
+    return render(request,"personal/home.html",content)
