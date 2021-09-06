231383246a2deac0b4ff00114709a3a08ca0fc15
mahdi <ssmmhh1382@gmail.com>
2021-09-06 19:49:33 +0430

    Django models and admin

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,22 +1,12 @@
 from django.shortcuts import render
-
+from personal.models import Question
 # Create your views here.
 
 def home_screan_view(request):
-    # ex) 1
-    # content = {}
-    # content["my_number"] = "sisad hezaro bist o char"
 
-    # ex) 2
-    # content = {
-    # "my_number": "charsad bist o char yea yea212",
-    # "khersi": "khersi is here \n his name is coli"
-    # }
 
-    # ex) 3
-    content = {}
-    listOfBooks = ["work harder","clean code","best program","khajebe"]
-    listOfBooks.append("TAMARKOZ")
-
-    content["listOfBooks"] = listOfBooks
+    questions = Question.objects.all()
+    content = {
+        "questions": questions
+    }
     return render(request,"personal/home.html",content)
