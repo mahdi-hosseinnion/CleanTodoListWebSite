9f9f72554e0d1a9687dd12c6c3241481a9ec8bd3
mahdi <ssmmhh1382@gmail.com>
2021-09-07 11:48:53 +0430

    create custom user model & customizing admin panel

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from personal.models import Question
+from account.models import Account
 # Create your views here.
 
 def home_screan_view(request):
-
-
-    questions = Question.objects.all()
-    content = {
-        "questions": questions
-    }
-    return render(request,"personal/home.html",content)
+    context = {}
+    accounts = Account.objects.all()
+    context["accounts"] = accounts
+    return render(request,"personal/home.html",context)
