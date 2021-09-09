1a1ad078246b95d9680286e65f7726107fe90ff4
mahdi <ssmmhh1382@gmail.com>
2021-09-09 14:50:54 +0430

    17- Update user account with forms

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -20,12 +20,14 @@ from personal.views import home_screan_view
 from account.views import (
     registration_view,
     login_view,
-    logout_view
+    logout_view,
+    account_view
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screan_view, name = 'home'),
     path('register/', registration_view , name = 'register'),
     path('logout/', logout_view , name = 'logout'),
-    path('login/', login_view , name = 'login')
+    path('login/', login_view , name = 'login'),
+    path('account/', account_view , name = 'account')
 ]
