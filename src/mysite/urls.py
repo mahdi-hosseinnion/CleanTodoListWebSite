516ffe8597f0924fec2fbc9093aed2c329188cad
mahdi <ssmmhh1382@gmail.com>
2021-09-09 12:17:19 +0430

    16- Logging in users

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -19,11 +19,13 @@ from personal.views import home_screan_view
 
 from account.views import (
     registration_view,
+    login_view,
     logout_view
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screan_view, name = 'home'),
     path('register/', registration_view , name = 'register'),
-    path('logout/', logout_view , name = 'logout')
+    path('logout/', logout_view , name = 'logout'),
+    path('login/', login_view , name = 'login')
 ]
